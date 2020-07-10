a0f4030f399c883912ecf7f928a0509e1ddc17fb
Jensen456 <62215937+Jensen456@users.noreply.github.com>
2020-07-10 12:31:55 -0400

    Added java-web-dev-studio7 Spinning Discs and associated files

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,10 +1,14 @@
 package org.launchcode.studio7;
 
-public class CD {
+public class CD extends BaseDisc implements IOpticalDisc {
 
     // TODO: Implement your custom interface.
-
+    public CD(String rpm) {
+        super(rpm);
+    }
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
-
+    public void spinDisc() {
+        System.out.println("A CD spins at a rate of " + this.rpm + " rpm.");
+    }
 }
--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,10 +1,14 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements IOpticalDisc {
 
     // TODO: Implement your custom interface.
-
+    public DVD(String rpm) {
+        super(rpm);
+    }
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
-
+    public void spinDisc() {
+        System.out.println("A DVD spins at a rate of " + this.rpm + " rpm.");
+    }
 }
